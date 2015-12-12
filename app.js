1e72fd1f3d9f35f5755a9bc4f03299eaf6d3268f
Glib Ischenko <Fleischer458@gmail.com>
2015-12-12 16:09:25 +0200

    Something with mongodb

--- app.js
+++ app.js
@@ -3,13 +3,37 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 var winston = require('winston');
+var favicon = require('serve-favicon');
 
-// app.get('/', function(req, res) {
-//     res.send('Hello World!')
-// })
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost/recooker');
 
-// app.get('/', routes.index);
+
+// routes
 app.use(express.static('www'));
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.get('/users', routes.users);
+app.get('/userlist', routes.userlist);
+
+// in case of error
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 var server = app.listen(8080, function() {
 
